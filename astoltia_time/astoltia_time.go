5f9b0bd5c5df1c068e17f24335223c7c97443c8d
cube3x3x3 <cube3x3x3@gmail.com>
2020-03-10 18:20:14 +0900

    write test code
    各関数向けのテストコードを書いた

--- astoltia_time/astoltia_time.go
+++ astoltia_time/astoltia_time.go
@@ -30,27 +30,6 @@ func AstNightsToMin(AstTime time.Time, nights int) float64 {
 }
 
 func main() {
-	daySec := DaySeconds(time.Now())
-	fmt.Println(daySec)
-	Adate := SecToAstoltiaTime(180)
-	fmt.Println("Astoltia time is ", Adate)
-	tomin := AstNightsToMin(Adate, 66)
-	fmt.Println(tomin)
-	fmt.Println(time.Now().Add(time.Duration(tomin) * time.Minute))
-
-	fmt.Println("Astoltia time is ", Adate)
-	fmt.Println("Astoltia time is ", SecToAstoltiaTime(int(daySec)))
-
-	ColossusTime := time.Date(2020, 3, 10, 7, 0, 0, 0, time.Local)
-	day, hour, min, sec := AstoltiaNights(ColossusTime)
-	fmt.Printf("Colossus time is %dNights %2d:%2d:%2d\n", day, hour, min, sec)
-	for i := 0; i < 5; i++ {
-		fmt.Println(ColossusTime.Add(time.Duration(AstNightsToMin(ColossusTime, i)) * time.Minute))
-	}
-	for i := 0; i < 660; i += 66 {
-		fmt.Println(ColossusTime.Add(time.Duration(AstNightsToMin(ColossusTime, i)) * time.Minute))
-	}
-
-	day, hour, min, sec = AstoltiaNights(time.Now())
+	day, hour, min, sec := AstoltiaNights(time.Now())
 	fmt.Printf("Astoltia time is %dNights %2d:%2d:%2d\n", day, hour, min, sec)
 }
