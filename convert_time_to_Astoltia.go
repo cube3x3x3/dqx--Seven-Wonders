ec083aae53075c7730a1f20767edd9a755119376
cube3x3x3 <cube3x3x3@gmail.com>
2020-03-10 14:14:07 +0900

    Update convert_time_to_Astoltia.go
    アストルティアで何夜後が何分後になるかの変換
    大巨人がでる時間と、その結果。
    次回の大巨人時間の表示

--- convert_time_to_Astoltia.go
+++ convert_time_to_Astoltia.go
@@ -22,12 +22,30 @@ func AstoltiaNights(setTime time.Time) (astDay int, astHour int, astMin int, ast
 	return AstTime.Day(), AstTime.Hour(), AstTime.Minute(), AstTime.Second()
 }
 
+func AstNightsToMin(AstTime time.Time, nights int) float64 {
+	// アストルティアの66夜後が何分後になるか
+	targetTime := AstTime.AddDate(0, 0, nights)
+	duration := targetTime.Sub(AstTime)
+	return duration.Minutes() / 20
+}
+
 func main() {
 	daySec := DaySeconds(time.Now())
 	fmt.Println(daySec)
 	Adate := SecToAstoltiaTime(180)
 	fmt.Println("Astoltia time is ", Adate)
+	tomin := AstNightsToMin(Adate, 66)
+	fmt.Println(tomin)
+	fmt.Println(time.Now().Add(time.Duration(tomin) * time.Minute))
+
+	fmt.Println("Astoltia time is ", Adate)
 	fmt.Println("Astoltia time is ", SecToAstoltiaTime(int(daySec)))
-	day, hour, min, sec := AstoltiaNights(time.Now())
-	fmt.Printf("Astoltia time is %dNights %d:%d:%d", day, hour, min, sec)
+
+	ColossusTime := time.Date(2020, 3, 10, 7, 0, 0, 0, time.Local)
+	day, hour, min, sec := AstoltiaNights(ColossusTime)
+	fmt.Printf("Colossus time is %dNights %2d:%2d:%2d\n", day, hour, min, sec)
+	fmt.Println(ColossusTime.Add(time.Duration(AstNightsToMin(ColossusTime, 1)) * time.Minute))
+
+	day, hour, min, sec = AstoltiaNights(time.Now())
+	fmt.Printf("Astoltia time is %dNights %2d:%2d:%2d\n", day, hour, min, sec)
 }
