7e59efc9e26710e84d0b10a7a899663390b4ba78
cube3x3x3 <cube3x3x3@gmail.com>
2020-03-10 11:59:26 +0900

    first func

--- convert_time_to_Astoltia.go
+++ convert_time_to_Astoltia.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func ConvertToAstoltiaTime(setTime time.Time) float64 {
+	y := setTime.Year()
+	m := setTime.Month()
+	d := setTime.Day()
+	initDay := time.Date(y, m, d, 0, 0, 0, 0, time.Local)
+	sub := setTime.Sub(initDay)
+	fmt.Println(y, m, d, initDay, sub)
+	AstoltiaTime := sub.Minutes() / 72
+	return AstoltiaTime
+}
 
 func main() {
-	fmt.Printf("Astoltia time is 00:00.\n")
+	Atime := ConvertToAstoltiaTime(time.Now())
+	fmt.Printf("Astoltia time is %f.\n", Atime)
+
 }
